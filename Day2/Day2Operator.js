e07d69f404779d01162f9a145b4cb63cfd12315a
Gauri <gauriM304@gmail.com>
2026-01-09 17:05:53 +0530

    Comparison operators in JS

--- Day2/Day2Operator.js
+++ Day2/Day2Operator.js
@@ -6,3 +6,16 @@ console.log(a-b);
 console.log(a*b);
 console.log(a/b);
 
+let x = 5;
+let y = "5";
+
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+
+
+
