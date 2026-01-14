9da4f34c2e9db3125df47641999ff06244a3f1e2
Gauri <gauriM304@gmail.com>
2026-01-14 20:54:22 +0530

    loops in JS

---
+++ Day4/Day4Loops.js
@@ -0,0 +1,17 @@
+for(let i=0; i<=5; i++){
+    //console.log(i)
+}
+
+let count = 4;
+
+while (count<=3){
+    console.log(count);
+    count++;
+}
+
+let skill= ["JavaScript" ,"Java " ,"Nodejs" ];
+
+for (let i=0; i<skill.length; i++){
+    console.log(skill[i])
+}
+
