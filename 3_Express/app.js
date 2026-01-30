b28ae755c7221e062789d021b4fa8506a777bc8d
Gauri <gauriM304@gmail.com>
2026-01-30 13:35:51 +0530

    app.js

--- 3_Express/app.js
+++ 3_Express/app.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/' ,(req , res) => {
-    res.send('Hello from gauri')
+app.get("/", (req, res) => {
+  res.send("Hello from gauri");
 });
 
-app.get('/about' ,(res, req) => {
-    res.send('create impact')
+app.get("/about", (req, res) => {
+  res.send("create impact");
 });
 
-app.listen(3000 , () => console.log("port running on 3000"));
+app.get("/contact", (req, res) => {
+  res.send("contact us");
+});
+
+app.listen(3000, () => console.log("port running on 3000"));
