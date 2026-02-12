cf2ea128fa21100777c1c1b6307b028d669d458e
Gauri <gauriM304@gmail.com>
2026-02-12 11:18:51 +0530

    interview preps

---
+++ Interview_Prep/output.js
@@ -0,0 +1,13 @@
+console.log("Start"); //synchronous
+
+setTimeout(() => {
+  console.log("Inside Timeout"); //web api , call back queue , event loop will check
+  // if the call stack is empty it will execevuted 
+}, 0);
+
+console.log("End"); // syunchronous 
+
+//output 
+//start
+//end
+//inside timeout
