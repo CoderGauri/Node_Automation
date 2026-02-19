e9045363864cc164c1a4462341d86c6a6c69706d
Gauri <gauriM304@gmail.com>
2026-02-19 12:58:03 +0530

    Basic API

--- APIs/Day1/index.js
+++ APIs/Day1/index.js
@@ -4,7 +4,13 @@ const app = express();
 app.get('/home',(req , res)=> {
     return res.send("This is a home Page");
 })
+app.get('/about',(req,res)=> {
+    return res.send("This is my first API");
+})
 
+app.get('/time',(req,res)=>{
+    return res.send(Date());
+})
 const PORT = 3000;
 app.listen(PORT ,()=> {
     console.log("app running at port 3000")
