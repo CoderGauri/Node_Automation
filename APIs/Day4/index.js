c079f4a72018362b80899f11015fc47eab8de495
Gauri <gauriM304@gmail.com>
2026-02-25 13:09:56 +0530

    Data.json file added

--- APIs/Day4/index.js
+++ APIs/Day4/index.js
@@ -1,27 +1,35 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 app.use(express.json());
 
-const Shop = [
-  { id: 1, pName: "pen", pPrice: 30 },
-  { id: 2, pName: "pencil", pPrice: 10 },
-];
-
 app.get("/", (req, res) => {
   return res.send("Node app in building");
 });
+const getProducts = () => {
+  const data = fs.readFileSync("data.json");
+  return JSON.parse(data);
+};
+
+const saveProducts = (products) => {
+  fs.writeFileSync("data.json", JSON.stringify(products, null, 2));
+};
 app.get("/Shop", (req, res) => {
-  return res.json(Shop);
+  const products = getProducts();
+  return res.json(products);
 });
 
-app.post('/Shop' , (req , res)=> {
-    const newProduct = req.body;
-    console.log("Incoming data :" ,newProduct);
-    Shop.push(newProduct);
-    res.status(201).json({
-        message:"product added successfully",
-        data:newProduct
-    })})
+app.post("/Shop", (req, res) => {
+  const products = getProducts();
+  const newProduct = req.body;
+  console.log("Incoming data :", newProduct);
+  products.push(newProduct);
+  saveProducts(products);
+  res.status(201).json({
+    message: "product added successfully",
+    data: newProduct,
+  });
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log("App running at PORT 3000");
