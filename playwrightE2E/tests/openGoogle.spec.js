3040c798d2efc8c3443232e2a9fc96e734b3ca52
Gauri <gauriM304@gmail.com>
2026-03-04 14:45:38 +0530

    headed test run

---
+++ playwrightE2E/tests/openGoogle.spec.js
@@ -0,0 +1,7 @@
+const {test , expect} = require('@playwright/test');
+
+test('open google' , async({page})=> {
+    await page.goto('https://google.com');
+    await page.fill('textarea[name="q"]', 'Playwright');
+    await page.press('textarea[name="q"]', 'Enter');
+});
