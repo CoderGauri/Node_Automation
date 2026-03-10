66b68e0c479ab3876e624bf59313ffefd62ae7aa
Gauri <gauriM304@gmail.com>
2026-03-10 14:14:18 +0530

    Login test cases

--- playwrightE2E/tests/login.spec.js
+++ playwrightE2E/tests/login.spec.js
@@ -1,5 +1,8 @@
 const {expect , test} = require('@playwright/test');
 
 test('login', async({Page})=> {
-    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await Page.goto('https://practicetestautomation.com/practice-test-login/');
+    await Page.locator("input[name='username']").type("student");
+    await Page.locator("input[name='password']").type("Password123")
+    await Page.locator("(//button[normalize-space()='Submit'])[1]").click();
 })
