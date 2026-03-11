a344cf775850e55d9b8d2e85c2dd4059a22543e9
Gauri <gauriM304@gmail.com>
2026-03-11 18:23:57 +0530

    Login test cases

--- playwrightE2E/tests/login.spec.js
+++ playwrightE2E/tests/login.spec.js
@@ -1,8 +1,15 @@
-const {expect , test} = require('@playwright/test');
+// 
+const { test, expect } = require('@playwright/test');
 
-test('login', async({Page})=> {
-    await Page.goto('https://practicetestautomation.com/practice-test-login/');
-    await Page.locator("input[name='username']").type("student");
-    await Page.locator("input[name='password']").type("Password123")
-    await Page.locator("(//button[normalize-space()='Submit'])[1]").click();
-})
+test("Valid Login", async ({ page }) => {
+
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+    await page.fill('#username', 'student');
+    await page.fill('#password', 'Password123');
+
+    await page.click('#submit');
+
+    await expect(page).toHaveURL(/logged-in-successfully/);
+
+});
