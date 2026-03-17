efd641124429e9f5c9bd9ef7c7af2b37c1f1e40a
Gauri <gaurim304@gmail.com>
2026-03-17 20:02:23 +0530

    Valid and Invalid scenarios added

--- playwright-login-framework/tests/loginTest.spec.js
+++ playwright-login-framework/tests/loginTest.spec.js
@@ -2,14 +2,14 @@ import {test , expect} from '@playwright/test'
 import {LoginPage} from '../pages/LoginPage'
 import {loginData} from '../utils/testData'
 
-// test('@smoke Valid Login Test',async({page})=>{
-// const loginPage = new LoginPage(page)
-// await loginPage.navigate()
-// await loginPage.login(loginData.username , loginData.password)
-// await expect(page.locator('.post-title')).toContainText('Logged In Successfully')
-// await expect(page.locator('.post-title')).toBeVisible()
-// await expect (page).toHaveURL(/logged-in-successfully/)
-// })
+test('@smoke Valid Login Test',async({page})=>{
+const loginPage = new LoginPage(page)
+await loginPage.navigate()
+await loginPage.login(loginData.username , loginData.password)
+await expect(page.locator('.post-title')).toContainText('Logged In Successfully')
+await expect(page.locator('.post-title')).toBeVisible()
+await expect (page).toHaveURL(/logged-in-successfully/)
+})
 
 // test('@regression  Invalid Login Test' , async({page})=>{
 //     const loginPage = new LoginPage(page)
