cdda96c905d6cbd1c73ae1afe5328819d7766bdb
Gauri <gauri2connect@gmail.com>
2026-04-21 12:24:16 +0530

    end to end project from scratch

---
+++ Playwright-framework/pages/LoginPage.js
@@ -0,0 +1,19 @@
+class LoginPage {
+    constructor(page){
+        this.page = page;
+        this.usernameInput = '#username';
+        this.passwordInput = '#password';
+        this.loginbtn = '#loginbtn';
+        this.dashboardText = '#dashboard'; 
+    }
+
+    async navigate(){
+        await this.page.goto('/login');
+    }
+
+    async login(username , password) {
+        await this.page.fill(this.usernameInput , username);
+        await this.page.fill(this.passwordInput , password);
+        await this.page.click(this.loginbtn );  
+    }
+}
